ef91f4ed50b4e219eecb8bb14e2936330c681829
gulingling <gulinglingcs@163.com>
2015-09-05 17:49:05 +0800

    relative layout

--- layout_manager/src/com/example/layout_manager/MainActivity.java
+++ layout_manager/src/com/example/layout_manager/MainActivity.java
@@ -15,59 +15,10 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 	
-	private int currentColor=0;
-	final int[] colors = new int[]{
-			R.color.color1,
-			R.color.color2,
-			R.color.color3,
-			R.color.color4,
-			R.color.color5,
-			R.color.color6
-	};
-	final int[] names = new int[]{
-			R.id.view01,
-			R.id.view02,
-			R.id.view03,
-			R.id.view04,
-			R.id.view05,
-			R.id.view06
-	};
-	
-	TextView[] views = new TextView[names.length];
-	Handler handler = new Handler()
-	{
-
-		@Override
-		public void handleMessage(Message msg) {
-			// TODO Auto-generated method stub
-			if(msg.what == 0x123)
-			{
-				for(int i=0;i<names.length;i++)
-				{
-					views[i].setBackgroundResource(colors[(i+currentColor)%names.length]);
-				}
-				currentColor++;
-			}
-			super.handleMessage(msg);
-		}
-	};
-	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for(int i=0;i<names.length;i++)
-        {
-        	views[i] = (TextView)findViewById(names[i]);
-        }
-        new Timer().schedule(new TimerTask()
-        {
-        	
-        	public void run()
-        	{
-        		handler.sendEmptyMessage(0x123);
-        	}
-        },0,2000);
     }
 
 
