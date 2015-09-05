fbd8bbf7eb850d9b5971dd242f676ba52e8cdaf2
gulingling <gulinglingcs@163.com>
2015-09-05 18:28:49 +0800

    end layout_manager

--- layout_manager/src/com/example/layout_manager/MainActivity.java
+++ layout_manager/src/com/example/layout_manager/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.layout_manager;
 
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.logging.LogRecord;
-
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.GridLayout;
+
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	GridLayout gridLayout;
+	String[] chars = new String[]
+			{
+				"7","8","9","/",
+				"4","5","6","*",
+				"1","2","3","-",
+				".","0","=","+",
+			};
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gridLayout = (GridLayout)findViewById(R.id.root);
+        for(int i=0;i<chars.length;i++)
+        {
+        	Button bn = new Button(this);
+        	bn.setText(chars[i]);
+        	bn.setTextSize(40);
+        	GridLayout.Spec rowSpec = GridLayout.spec(i/4+2);
+        	GridLayout.Spec columnSpec=GridLayout.spec(i%4);
+        	GridLayout.LayoutParams params=new GridLayout.LayoutParams(rowSpec,columnSpec);
+        	params.setGravity(Gravity.FILL);
+        	gridLayout.addView(bn,params);
+        }
     }
 
 
