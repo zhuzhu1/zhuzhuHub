22288d15a473cf74332e096de233055b543e46b4
gulingling <gulinglingcs@163.com>
2015-09-05 19:01:07 +0800

    test textview one

--- layout_manager/src/com/example/layout_manager/MainActivity.java
+++ layout_manager/src/com/example/layout_manager/MainActivity.java
@@ -13,31 +13,12 @@ import android.widget.GridLayout;
 
 public class MainActivity extends ActionBarActivity {
 	
-	GridLayout gridLayout;
-	String[] chars = new String[]
-			{
-				"7","8","9","/",
-				"4","5","6","*",
-				"1","2","3","-",
-				".","0","=","+",
-			};
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gridLayout = (GridLayout)findViewById(R.id.root);
-        for(int i=0;i<chars.length;i++)
-        {
-        	Button bn = new Button(this);
-        	bn.setText(chars[i]);
-        	bn.setTextSize(40);
-        	GridLayout.Spec rowSpec = GridLayout.spec(i/4+2);
-        	GridLayout.Spec columnSpec=GridLayout.spec(i%4);
-        	GridLayout.LayoutParams params=new GridLayout.LayoutParams(rowSpec,columnSpec);
-        	params.setGravity(Gravity.FILL);
-        	gridLayout.addView(bn,params);
-        }
+       
     }
 
 
