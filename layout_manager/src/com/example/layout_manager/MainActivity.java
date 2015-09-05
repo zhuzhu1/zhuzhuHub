93eff4dd2b89eb3f0190afd2b63d2578130e8a35
gulingling <gulinglingcs@163.com>
2015-09-05 17:36:26 +0800

    neon light

--- layout_manager/src/com/example/layout_manager/MainActivity.java
+++ layout_manager/src/com/example/layout_manager/MainActivity.java
@@ -1,17 +1,73 @@
 package com.example.layout_manager;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.LogRecord;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	private int currentColor=0;
+	final int[] colors = new int[]{
+			R.color.color1,
+			R.color.color2,
+			R.color.color3,
+			R.color.color4,
+			R.color.color5,
+			R.color.color6
+	};
+	final int[] names = new int[]{
+			R.id.view01,
+			R.id.view02,
+			R.id.view03,
+			R.id.view04,
+			R.id.view05,
+			R.id.view06
+	};
+	
+	TextView[] views = new TextView[names.length];
+	Handler handler = new Handler()
+	{
 
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			if(msg.what == 0x123)
+			{
+				for(int i=0;i<names.length;i++)
+				{
+					views[i].setBackgroundResource(colors[(i+currentColor)%names.length]);
+				}
+				currentColor++;
+			}
+			super.handleMessage(msg);
+		}
+	};
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for(int i=0;i<names.length;i++)
+        {
+        	views[i] = (TextView)findViewById(names[i]);
+        }
+        new Timer().schedule(new TimerTask()
+        {
+        	
+        	public void run()
+        	{
+        		handler.sendEmptyMessage(0x123);
+        	}
+        },0,2000);
     }
 
 
