faef4af211a6701f2e0e273d5ec1fd2271595de6
gulingling <gulinglingcs@163.com>
2015-09-05 21:23:37 +0800

    switch and toggleButton

--- TextViewTest/src/com/example/textviewtest/MainActivity.java
+++ TextViewTest/src/com/example/textviewtest/MainActivity.java
@@ -4,14 +4,48 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	ToggleButton toggle;
+	Switch switcher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        toggle = (ToggleButton)findViewById(R.id.toggle);
+        switcher = (Switch)findViewById(R.id.switcher);
+        final LinearLayout test = (LinearLayout)findViewById(R.id.test);
+        OnCheckedChangeListener listener = new OnCheckedChangeListener()
+        {
+
+			@Override
+			public void onCheckedChanged(CompoundButton button, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if(isChecked)
+				{
+					//设置LinearLayout垂直布局
+					test.setOrientation(1);
+				}
+				else
+				{
+					//设置LinearLayout水平布局
+					test.setOrientation(0);
+				}
+			}
+
+        	
+        };
+        toggle.setOnCheckedChangeListener(listener);
+        switcher.setOnCheckedChangeListener(listener);
+        
     }
 
 
